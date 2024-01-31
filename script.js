09f93da7595e5d9e73c84da4adc59db290ed670a
Ollie-Judge <ollie.judge@hotmail.com>
2024-01-31 14:33:38 +0000

    added a timer and fixed red text not being black

--- script.js
+++ script.js
@@ -1,21 +1,29 @@
 const quoteToCopy = document.getElementById("quoteToCopy");
 const quoteInput = document.getElementById("quoteInput");
+const timer = document.getElementById("timer");
 
 quoteInput.addEventListener("input", (e) => {
   const arrayQuoteToCopy = quoteToCopy.querySelectorAll("span");
   const userInputQuote = e.target.value.split("");
-  console.log("quote: ", arrayQuoteToCopy);
-  console.log("user: ", userInputQuote);
+
+  let correct = true;
+
   arrayQuoteToCopy.forEach((characterSpan, i) => {
     const character = userInputQuote[i];
-    if (character === characterSpan.innerText) {
+    if (character == null) {
+      characterSpan.classList.remove("incorrect");
+      characterSpan.classList.remove("correct");
+      correct = false;
+    } else if (character === characterSpan.innerText) {
       characterSpan.classList.add("correct");
       characterSpan.classList.remove("incorrect");
     } else {
       characterSpan.classList.add("incorrect");
       characterSpan.classList.remove("correct");
+      correct = false;
     }
   });
+  if (correct) getNextQuote();
 });
 
 const randomQuoteApiUrl = "http://api.quotable.io/random";
@@ -27,6 +35,7 @@ const getRandomQuote = () => {
 };
 
 const getNextQuote = async () => {
+  startTimer();
   const quote = await getRandomQuote();
   quoteToCopy.innerHTML = "";
   quote.content.split("").forEach((character) => {
@@ -34,6 +43,20 @@ const getNextQuote = async () => {
     characterSpan.innerText = character;
     quoteToCopy.appendChild(characterSpan);
   });
+};
+
+let startTime;
+
+const startTimer = () => {
+  timer.innerText = 0;
+  startTime = new Date();
+  setInterval(() => {
+    timer.innerText = getTimerTime();
+  }, 1000);
+};
+
+const getTimerTime = () => {
+  return Math.floor((new Date() - startTime) / 1000);
 };
 
 getNextQuote();
