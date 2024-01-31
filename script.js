ce2d2984903f8b0bcb77ebeddcbce9b3e8f51a31
Ollie-Judge <ollie.judge@hotmail.com>
2024-01-31 13:37:24 +0000

    added placeholder and got valueof the input

--- script.js
+++ script.js
@@ -1,4 +1,10 @@
 const quoteToCopy = document.getElementById("quoteToCopy");
+const quoteInput = document.getElementById("quoteInput");
+
+quoteInput.addEventListener("input", (e) => {
+  console.log(e.target.value);
+  //characterSpan.classList.add("incorrect");
+});
 
 const randomQuoteApiUrl = "http://api.quotable.io/random";
 
@@ -10,8 +16,12 @@ const getRandomQuote = () => {
 
 const getNextQuote = async () => {
   const quote = await getRandomQuote();
-  quoteToCopy.innerHTML = quote.content;
-  console.log(quote);
+  quoteToCopy.innerHTML = "";
+  quote.content.split("").forEach((character) => {
+    const characterSpan = document.createElement("span");
+    characterSpan.innerText = character;
+    quoteToCopy.appendChild(characterSpan);
+  });
 };
 
 getNextQuote();
